2c99cdcf751fd002d3b6c635f17a3d350a4eb2de
AkashAsane <akashasane4455@gmail.com>
2024-11-20 20:26:10 +0530

    utils updated

--- frontend/src/utils/getBaseUrl.js
+++ frontend/src/utils/getBaseUrl.js
@@ -1,6 +1,6 @@
 const getBaseUrl = () => {
     // return "http://localhost:5000"
-    return "https://book-store-backend-six-alpha.vercel.app"
+    return "https://bookstore-1b20.onrender.com"
 }
 
 export default getBaseUrl;
