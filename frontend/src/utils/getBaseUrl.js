2bc42b21e805b12ace8d464c830b3c2d95536893
Tejaswi Rastogi <tejaswi376@gmail.com>
2024-11-20 08:11:01 +0530

    Bug fixes

--- frontend/src/utils/getBaseUrl.js
+++ frontend/src/utils/getBaseUrl.js
@@ -1,6 +1,6 @@
 const getBaseUrl = () => {
     // return "http://localhost:5000"
-    return "https://book-store-backend-six-alpha.vercel.app/"
+    return "https://book-store-backend-six-alpha.vercel.app"
 }
 
 export default getBaseUrl;
